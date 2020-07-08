63843060cbf71926280b80ea66c2f2d8ab3fe3eb
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-08 15:30:08 +0200

    Add background image for large screens and finish functionality for hamburger menu.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const hamClose = document.querySelector('.close')
 
 hamburger.addEventListener('click', () => {
   const div = document.createElement('div');
   div.className = 'ham-container';
   div.innerHTML = `
-  
-  <span>+</span>
+  <span class="close">+</span>
     <div class="ham-content">
       <ul>
         <li>Home</li>
@@ -15,4 +15,13 @@ hamburger.addEventListener('click', () => {
     </div>
   `
   document.body.appendChild(div);
+  hamburger.style.display = 'none';
+})
+
+document.addEventListener('click', (e) => {
+  const div = document.querySelector('.ham-container');
+  if (e.target.className == 'close') {
+    div.remove();
+    hamburger.style.display = 'block';
+  } 
 })
