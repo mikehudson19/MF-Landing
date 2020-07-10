43e9f5499b4ae26d5ece5480918a6489215726c3
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-10 18:27:00 +0200

    Refactor to implement BEM CSS methodology.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.navbar__hamburger');
 const hamClose = document.querySelector('.close')
 
 hamburger.addEventListener('click', () => {
   const div = document.createElement('div');
-  div.className = 'ham-container';
+  div.className = 'ham-nav';
   div.innerHTML = `
-  <span class="close">+</span>
-    <div class="ham-content">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+  <span class="ham-nav__close">+</span>
+    <div class="ham-nav__content">
+      <ul class="ham-nav__list">
+        <li class="ham-nav__item">Home</li>
+        <li class="ham-nav__item">About</li>
+        <li class="ham-nav__item">Contact</li>
       </ul>
     </div>
   `
@@ -19,8 +19,8 @@ hamburger.addEventListener('click', () => {
 })
 
 document.addEventListener('click', (e) => {
-  const div = document.querySelector('.ham-container');
-  if (e.target.className == 'close') {
+  const div = document.querySelector('.ham-nav');
+  if (e.target.className == 'ham-nav__close') {
     div.remove();
     hamburger.style.display = 'block';
   } 
